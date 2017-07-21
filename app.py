b5a1a4c8c84ea8ce6c86264162f88c5b9915954b
anton <galkin88@gmail.com>
2017-07-21 16:27:44 +0500

    Add some views

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.response import Response
-
-def hello_world(request):
-    url = request.url
-    name = request.params.get('name', 'No name provided');
-    body = 'URL %s with name: %s' % (url, name)
-    return Response(
-        content_type="text/plain",
-        body=body
-    )
 
 if __name__ == '__main__':
     with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+        config.scan('views')
+        config.add_route('home', '/')
+        config.add_route('hello', '/howdy')
+        config.add_route('redirect', '/goto')
+        config.add_route('exception', '/problem')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
